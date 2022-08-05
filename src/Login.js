776599a118ddd340f22379a376243f16a88c95d1
ayebaishmo <ishmo255super@gmail.com>
2022-08-05 15:58:56 +0600

    Start

---
+++ src/Login.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './login.css';
+
+function Login() {
+  return (
+    <div className="App">
+      <div>
+        <div>
+          <h1>Chimpnzee Trust</h1>
+          <h2>Login</h2>
+          <ul>
+            <li>
+              <label htmlFor="username">Username</label>
+              <input type="text" />
+            </li>
+            <li>
+              <label htmlFor="password">Password</label>
+              <input type="text" />
+            </li>
+            <li>
+              <button>Login</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
